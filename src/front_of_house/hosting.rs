e591972bd67a8977e07a109d3d001eedd140f1ab
James Titcumb <james@asgrim.com>
2020-06-24 08:14:28 +0100

    Playing iwth modules and whatnot

---
+++ src/front_of_house/hosting.rs
@@ -0,0 +1,5 @@
+pub fn add_to_waitlist() {
+    crate::back_of_house::cook_order();
+}
+
+// fn seat_at_table() {}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-mod enums;
+//mod enums;
 // mod game;
 // mod ownership;
 // mod structs;
+mod back_of_house;
+mod front_of_house;
+mod structs;
+
+use crate::front_of_house::hosting as Hello;
 
 fn main() {
     // game::run();
     // ownership::run();
-    // structs::run();
-    enums::run();
+    structs::run();
+    // enums::run();
+    Hello::add_to_waitlist();
 }
